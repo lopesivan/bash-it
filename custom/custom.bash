c1c0ed983eb5fa940aeba92be77d33fefd2f1044
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-04-25 17:59:20 -0300

    Atualiza o arquivo custom/custom.bash

--- custom/custom.bash
+++ custom/custom.bash
@@ -11,9 +11,3 @@ export PAGER=more
 #    vim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' \
 #    -c 'map <SPACE> <C-D>' -c 'map b <C-U>' \
 #    -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\""
-
-govim()
-{
-    cd ${HOME}/developer/vim/vim
-}
-
