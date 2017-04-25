b3c87e3872737edbd459314f88053eafd2a88909
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-04-25 17:59:21 -0300

    Atualiza o arquivo plugins/available/jenv.plugin.bash

--- plugins/available/jenv.plugin.bash
+++ plugins/available/jenv.plugin.bash
@@ -4,6 +4,10 @@ about-plugin 'load jenv, if you are using it'
 export JENV_ROOT="$HOME/.jenv"
 pathmunge "$JENV_ROOT/bin"
 
-if which jenv > /dev/null; then eval "$(jenv init -)"; fi
+if which jenv > /dev/null; then
+    eval "$(jenv init -)";
+    export JAVA_HOME="$HOME/.jenv/versions/`jenv version-name`"
+    alias jenv_set_java_home='export JAVA_HOME="$HOME/.jenv/versions/`jenv version-name`"'
+fi
 
 [[ -e $JENV_ROOT/completions/jenv.bash ]] && source $JENV_ROOT/completions/jenv.bash
