3d0484b2d7eee42b119624c57a6a624ecf94d563
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-04-27 16:04:37 -0300

    Atualiza o arquivo custom/environment.bash

--- custom/environment.bash
+++ custom/environment.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # export PROJECT_PATHS=~/workspace:~/developer
-export PROJECT_PATHS=~/workspace
+#export PROJECT_PATHS=~/workspace
+export PROJECT_PATHS=/workspace/git-recipes/branches/
