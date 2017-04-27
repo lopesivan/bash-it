73aec1482c1bb5cd0f9f6e85e86213ae3687af06
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-04-27 02:31:04 -0300

    Atualiza o arquivo aliases/available/vim.aliases.bash

--- aliases/available/vim.aliases.bash
+++ aliases/available/vim.aliases.bash
@@ -1,19 +1,25 @@
 cite 'about-alias'
 about-alias 'vim abbreviations'
 
-VIM=$(command -v vim)
-GVIM=$(command -v gvim)
-MVIM=$(command -v mvim)
 
-[[ -n $VIM ]] && alias v=$VIM
-
-# open vim in new tab is taken from
-# http://stackoverflow.com/questions/936501/let-gvim-always-run-a-single-instancek
 case $OSTYPE in
-  darwin*)
-	[[ -n $MVIM ]] && function mvimt { command mvim --remote-tab-silent "$@" || command mvim "$@"; }
-    ;;
-  *)
-    [[ -n $GVIM ]] && function gvimt { command gvim --remote-tab-silent "$@" || command gvim "$@"; }
+  linux*)
+     VIM=$(command -v vim)
+    NVIM=$(command -v nvim)
+    GVIM=$(command -v gvim)
+    NODE=$(command -v node)
+    XCLIP=$(command -v xclip)
+
+    [[ $NVIM ]] &&
+      alias vim-install-plug="$NVIM +PlugInstall +qall" &&
+        alias vim-install-rplug="$NVIM +UpdateRemotePlugins +qall" &&
+          alias vim-cheetah="$NVIM -c:Cheetah" &&
+            [[ $XCLIP ]] &&
+              alias vim-xpaste='vim "$(xpaste)"'
+
+    [[ -n $GVIM ]] &&
+      function gvimt { command gvim --remote-tab-silent "$@" || command gvim "$@"; }
+
     ;;
 esac
+
