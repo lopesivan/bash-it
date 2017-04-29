86bdc17ca31f3f292d9a978c49caa3e6b7973696
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-04-29 14:45:19 -0300

    Atualiza o arquivo aliases/available/vim.aliases.bash

--- aliases/available/vim.aliases.bash
+++ aliases/available/vim.aliases.bash
@@ -10,12 +10,17 @@ case $OSTYPE in
     NODE=$(command -v node)
     XCLIP=$(command -v xclip)
 
-    [[ $NVIM ]] &&
-      alias vim-install-plug="$NVIM +PlugInstall +qall" &&
-        alias vim-install-rplug="$NVIM +UpdateRemotePlugins +qall" &&
-          alias vim-cheetah="$NVIM -c:Cheetah" &&
-            [[ $XCLIP ]] &&
-              alias vim-xpaste='vim "$(xpaste)"'
+    [[ $NVIM ]] && {
+      alias vim-install-plug="$NVIM +PlugInstall +qall";
+      alias vim-install-rplug="$NVIM +UpdateRemotePlugins +qall";
+      alias vim-cheetah="$NVIM -c:Cheetah";
+      alias vc="$NVIM -c:Cheetah";
+    }
+    [[ $XCLIP ]] && {
+      alias vim-xclip='vim "$(xclip -selection clipboard -o)"';
+      alias vim-xclip-p='vim "$(xclip -selection primary -o)"';
+      alias vim-xclip-s='vim "$(xclip -selection secundary -o)"';
+    }
 
     [[ -n $GVIM ]] &&
       function gvimt { command gvim --remote-tab-silent "$@" || command gvim "$@"; }
